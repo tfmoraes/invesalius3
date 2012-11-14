97c92b53bad28e443d5bd2f8ee36767a4b759907
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-11-14 13:39:35 -0200

    Stacking in reverse way slices from a coronal serie

--- invesalius/data/imagedata_utils.py
+++ invesalius/data/imagedata_utils.py
@@ -474,7 +474,7 @@ def dcm2memmap(files, slice_size, orientation, resolution_percentage):
         array = numpy_support.vtk_to_numpy(image.GetPointData().GetScalars())
         if orientation == 'CORONAL':
             array.shape = matrix.shape[0], matrix.shape[2]
-            matrix[:, n, :] = array
+            matrix[:, -n-1, :] = array
         elif orientation == 'SAGITTAL':
             array.shape = matrix.shape[0], matrix.shape[1]
             # TODO: Verify if it's necessary to add the slices swapped only in
