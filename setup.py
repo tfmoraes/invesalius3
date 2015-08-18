b1a7a6657e63892851561fb4b090871e8d2979a1
Thiago Franco de Moraes <totonixsame@gmail.com>
2015-08-18 14:32:19 -0300

    Added smooth_cy (only the algorithm) to InVesalius

--- setup.py
+++ setup.py
@@ -11,12 +11,14 @@ import numpy
 if sys.platform == 'linux2':
     setup(
         cmdclass = {'build_ext': build_ext},
-        ext_modules = cythonize([ Extension("invesalius.data.mips", ["invesalius/data/mips.pyx"],
+        ext_modules = cythonize([ Extension("invesalius.data.mips",
+                                            ["invesalius/data/mips.pyx"],
                                             include_dirs =  [numpy.get_include()],
                                             extra_compile_args=['-fopenmp'],
                                             extra_link_args=['-fopenmp']),
 
-                                 Extension("invesalius.data.interpolation", ["invesalius/data/interpolation.pyx"],
+                                 Extension("invesalius.data.interpolation",
+                                           ["invesalius/data/interpolation.pyx"],
                                            include_dirs=[numpy.get_include()],
                                            extra_compile_args=['-fopenmp',],
                                            extra_link_args=['-fopenmp',]),
@@ -29,10 +31,6 @@ if sys.platform == 'linux2':
                        Extension("invesalius.data.floodfill", ["invesalius/data/floodfill.pyx"],
                                  include_dirs=[numpy.get_include()],
                                  language='c++',),
-                                 Extension("invesalius.data.transforms", ["invesalius/data/transforms.pyx"],
-                                           include_dirs=[numpy.get_include()],
-                                           extra_compile_args=['-fopenmp',],
-                                           extra_link_args=['-fopenmp',]),
 
                                  # "Reducing Aliasing Artifacts in Iso-Surfaces of Binary Volumes"
                                  Extension("invesalius.data.smooth_cy",
