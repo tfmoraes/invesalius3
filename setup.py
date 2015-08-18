6716954c39efab9846c28e8562f28e5b6275e315
Thiago Franco de Moraes <totonixsame@gmail.com>
2015-08-18 14:31:09 -0300

    Added smooth_cy (only the algorithm) to InVesalius

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ if sys.platform == 'linux2':
     setup(
         cmdclass = {'build_ext': build_ext},
         ext_modules = cythonize([ Extension("invesalius.data.mips", ["invesalius/data/mips.pyx"],
-                                  include_dirs =  [numpy.get_include()],
-                                  extra_compile_args=['-fopenmp'],
-                                  extra_link_args=['-fopenmp']),
+                                            include_dirs =  [numpy.get_include()],
+                                            extra_compile_args=['-fopenmp'],
+                                            extra_link_args=['-fopenmp']),
 
-                       Extension("invesalius.data.interpolation", ["invesalius/data/interpolation.pyx"],
-                                 include_dirs=[numpy.get_include()],
-                                 extra_compile_args=['-fopenmp',],
-                                 extra_link_args=['-fopenmp',]),
+                                 Extension("invesalius.data.interpolation", ["invesalius/data/interpolation.pyx"],
+                                           include_dirs=[numpy.get_include()],
+                                           extra_compile_args=['-fopenmp',],
+                                           extra_link_args=['-fopenmp',]),
 
                        Extension("invesalius.data.transforms", ["invesalius/data/transforms.pyx"],
                                  include_dirs=[numpy.get_include()],
@@ -29,8 +29,20 @@ if sys.platform == 'linux2':
                        Extension("invesalius.data.floodfill", ["invesalius/data/floodfill.pyx"],
                                  include_dirs=[numpy.get_include()],
                                  language='c++',),
-                       ])
-         )
+                                 Extension("invesalius.data.transforms", ["invesalius/data/transforms.pyx"],
+                                           include_dirs=[numpy.get_include()],
+                                           extra_compile_args=['-fopenmp',],
+                                           extra_link_args=['-fopenmp',]),
+
+                                 # "Reducing Aliasing Artifacts in Iso-Surfaces of Binary Volumes"
+                                 Extension("invesalius.data.smooth_cy",
+                                           ["invesalius/data/smooth_cy.pyx"],
+                                           include_dirs=[numpy.get_include()],
+                                           extra_compile_args=['-fopenmp'],
+                                           extra_link_args=['-fopenmp'],),
+
+                                 ])
+    )
 
 elif sys.platform == 'win32':
     setup(
